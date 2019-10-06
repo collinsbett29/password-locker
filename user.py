fa39d9fe49bca55b9f6500ef05377568a0b8b5f2
collinsbett29 <collinsbett29@gmail.com>
2019-10-06 21:09:49 +0300

    Add new modified changes

--- user.py
+++ user.py
@@ -1,12 +1,56 @@
-   class User:
+class User:
     """
-    Class that generates new instances of the user
+    Class that generates new instances of the user.
     """
-    def __init__(self,passwordlocker_details,username,password):
-        '''
-        __init__ method that helps us define properties for our objects.
-        '''
-            self.passwordlocker_details: User password locker details.
-            self.username : User username.
-            self.password: User password.
 
+    user_list = [] # Empty contact list
+
+    def __init__(self,passwordlocker_details,username,password):
+      """
+      Helps define properties the objects.
+      """
+      self.passwordlocker_details = details
+      self.username = username
+      self.user_password = password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class User:
+#     """
+#     Class that generates new instances of the user.
+#     """
+
+#     user_list = []
+
+#     def __init__(self,passwordlocker_details,username,password)):
+#       """
+#       Helps define properties the objects.
+#       """
+#         self.passwordlocker_details = details
+#         self.username = username
+#         self.user password = password
