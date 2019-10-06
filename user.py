ca72f1a7d038a1f83c5344567cbf71ceaea9817a
collinsbett29 <collinsbett29@gmail.com>
2019-10-06 12:27:51 +0300

    modify user.py file

--- user.py
+++ user.py
@@ -1,6 +1,11 @@
    class User:
     """
-    Class that generates new instances of contacts
+    Class that generates new instances of the user
     """
-
-    pass
+    def __init__(self,passwordlocker_details,username,password):
+        '''
+        __init__ method that helps us define properties for our objects.
+        '''
+            self.passwordlocker_details: User password locker details.
+            self.username : User username.
+            self.password: User password.
