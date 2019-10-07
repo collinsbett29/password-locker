dc57f97df99226b3423269ca4b2bd796b2ba3079
collinsbett29 <collinsbett29@gmail.com>
2019-10-07 12:07:03 +0300

    Update user.testr.py file

--- user.test.py
+++ user.test.py
@@ -13,12 +13,10 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("Collins Bett","##@@collins1")
 
-
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
-
         self.assertEqual(self.new_user.username,"Collins Bett")
         self.assertEqual(self.new_user.password,"##@@collins1")
 
@@ -30,7 +28,7 @@ class TestCredentials(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = Credentials("instagram") # create contact object
+        self.new_credentials = Credentials("instagram","##@@collins1") # creating credentials object
 
 
     def test_init(self):
@@ -38,8 +36,34 @@ class TestCredentials(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_credentials.username,"James")
-        self.assertEqual(self.new_credentials.password,"##@@james1"
+        self.assertEqual(self.new_credentials.credentials,"instagram")
+        self.assertEqual(self.new_credentials.password,"##@@collins1")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_store_credentials(self):
+        '''
+        test_store_credentials test case to test if the credentials object is stored into
+         the credentials list
+        '''
+        self.new_credentials.store_credentials() # saving the new credentials
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_display_all_credentials(self):
+            '''
+            method that returns a list of all contacts saved
+            '''
+
+            self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list) 
+
+    def test_delete_credentials(self):
+            '''
+            test_delete_credentials to test if we can remove a credentials from our credentials list
+            '''
+            self.new_credentials.store_credentials()
+            test_credentials = Credentials("instagram","##@@collins1") # new credential
+            test_credentials.store_credentials()
+
+            self.new_credentials.test_delete_credentials()# Deleting a credentials object
+            self.assertEqual(len(Credentials.credentials_list),3)
+
+if __name__ ==  '__main__':
+    unittest.main()        
