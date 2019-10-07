2b475b2c1690d6e6aad903ee38c055f2e8c0a5c7
collinsbett29 <collinsbett29@gmail.com>
2019-10-07 11:09:15 +0300

    Test credential class

--- user.test.py
+++ user.test.py
@@ -1,2 +1,45 @@
 import unittest
-from contact import Contact
+from user import User
+from user import Credentials
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Collins Bett","##@@collins1")
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Collins Bett")
+        self.assertEqual(self.new_user.password,"##@@collins1")
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = Credentials("instagram") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.username,"James")
+        self.assertEqual(self.new_credentials.password,"##@@james1"
+
+if __name__ == '__main__':
+    unittest.main()
