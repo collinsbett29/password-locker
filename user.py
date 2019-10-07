26073a0c81dcf5d23dc9b5869fe1593cf799c347
collinsbett29 <collinsbett29@gmail.com>
2019-10-07 11:07:46 +0300

    Add Credential class

--- user.py
+++ user.py
@@ -3,15 +3,45 @@ class User:
     Class that generates new instances of the user.
     """
 
-    user_list = [] # Empty contact list
+    user_list = []
 
-    def __init__(self,passwordlocker_details,username,password):
+    def __init__(self,username,password):
       """
       Helps define properties the objects.
       """
-      self.passwordlocker_details = details
       self.username = username
-      self.user_password = password
+      self.password = password
+
+
+
+class Credentials:
+    """
+    class that generates new instances of the credentials
+    """
+    credentials_list = []
+
+    def __init__(self,twitter,facebook,email,instagram,messenger,likedin,youtube,pinterest):
+      """
+      Helps define properties the objects.
+      """
+      self.twitter = password
+      self.facebook = password
+      self.email = password
+      self.instagram = password
+      self.messenger = password
+      self.likedin = password
+      self.youtube = password
+      self.pinterest = password
+
+
+
+
+
+
+
+
+
+
 
 
 
