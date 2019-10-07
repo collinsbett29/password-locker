d1be0082e9325d28219169ba3e1e2bb3dfb9c34f
collinsbett29 <collinsbett29@gmail.com>
2019-10-07 12:38:39 +0300

    create functions that allow user create credential.delete credential and store credential

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3.6
 from user import User
 from user import Credentials
+
+def create_credentials(Credentials,password):
+    '''
+    Function to create a new credential
+    '''
+    new_credentials = Credentials(Credentials,password)
+    return new_credentials
+
+def store_credentials(redentials):
+    '''
+    Function to store credentials
+    '''
+    Credentials.store_credentials() 
+
+def del_credentials(Credentials):
+    '''
+    Function to delete a credential
+    '''
+    Credentials.delete_credentials()       
