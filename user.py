dd316dfc1e14af1c411a81b8071606a38f9d165a
collinsbett29 <collinsbett29@gmail.com>
2019-10-07 12:05:55 +0300

    Update user.py file

--- user.py
+++ user.py
@@ -12,26 +12,47 @@ class User:
       self.username = username
       self.password = password
 
-
-
 class Credentials:
     """
     class that generates new instances of the credentials
     """
     credentials_list = []
 
-    def __init__(self,twitter,facebook,email,instagram,messenger,likedin,youtube,pinterest):
+    def __init__(self,credentials,password):
       """
       Helps define properties the objects.
       """
-      self.twitter = password
-      self.facebook = password
-      self.email = password
-      self.instagram = password
-      self.messenger = password
-      self.likedin = password
-      self.youtube = password
-      self.pinterest = password
+      self.credentials = credentials
+      self.password = password
+
+    def store_credentials(self):
+
+      '''
+      store_credentials method stores credentials objects into credentials_list
+      '''
+
+      Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+
+        '''
+        delete_credentials method deletes a credential that is no longer needed from the contact_list
+        '''
+
+        Credentials.credentials_list.remove(self)      
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        method that returns the credentials list
+        '''
+        return cls.credentials_list      
+
+
+
+
+
+
 
 
 
