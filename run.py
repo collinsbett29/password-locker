0790c71f7d45da7c6dbbdcc132bb1b605ce241a6
collinsbett29 <collinsbett29@gmail.com>
2019-10-07 15:54:25 +0300

    Add conditions to the file

--- run.py
+++ run.py
@@ -25,4 +25,45 @@ def display_credentials():
     '''
     Function that returns all the stored credentials
     '''
-    return Credentials.display_credentials()          
+    return Credentials.display_credentials()  
+
+def main():
+    print("Regiser")
+    print('\n')
+
+    while True:
+            print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+
+            short_code = input().lowe()
+
+            if short_code == 'cc'
+                    print("Register")
+
+                    print (Username.....)
+                    username = input()
+
+                    print(Password....)
+                    password = input()
+
+
+                    save_contacts(create_credentials(Credentials,password)) # create and save new credential.
+                            print ('\n')
+                            print(f"New Credentials {credentials} {password} created")
+                            print ('\n')
+
+
+                    elif short_code == 'dc':
+
+                            if display_credentials():
+                                    print("Here are all your credentials")
+                                    print('\n')
+
+                                    for credentials in display_credentials():
+                                            print(f"{credentials.credentials} {credentials.password}")
+
+                                    print('\n')
+                            else:
+                                    print('\n')
+                                    print("Your credentials list is empty")
+                                    print('\n')
+        
