59671620d58ed684b6a8272f2c8656043450696e
collinsbett29 <collinsbett29@gmail.com>
2019-10-08 09:41:31 +0300

    Add random password

--- run.py
+++ run.py
@@ -2,6 +2,14 @@
 from user import User
 from user import Credentials
 
+def generate_temp_password(length):
+    if not isinstance(length, int) or length < 8:
+        raise ValueError("temp password must have positive length")
+
+    chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+    from os import urandom
+    return "".join(chars[ord(c) % len(chars)] for c in urandom(length))
+
 def create_credentials(Credentials,password):
     '''
     Function to create a new credential
@@ -28,32 +36,38 @@ def display_credentials():
     return Credentials.display_credentials()  
 
 def main():
-    print("Regiser")
+    print("Register")
     print('\n')
 
+    print("username")
+    username = input()
+
+    print("password")
+    password = input()
+
+
+
     while True:
-            print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+            print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit the credential list ")
 
-            short_code = input().lowe()
+            short_code = input().lower()
 
-            if short_code == 'cc'
-                    print("Register")
+            if short_code == 'cc':
+                    print("Login")
 
-                    print (Username.....)
+                    print (Username)
                     username = input()
 
-                    print(Password....)
+                    print(Password)
                     password = input()
 
 
-                    save_contacts(create_credentials(Credentials,password)) # create and save new credential.
-                            print ('\n')
-                            print(f"New Credentials {credentials} {password} created")
-                            print ('\n')
-
+                    save_credentials(create_credentials(Credentials,password)) # create and save new credential.
+                    print ('\n')
+                    print(f"New Credential created")
+                    print ('\n')
 
                     elif short_code == 'dc':
-
                             if display_credentials():
                                     print("Here are all your credentials")
                                     print('\n')
@@ -66,4 +80,25 @@ def main():
                                     print('\n')
                                     print("Your credentials list is empty")
                                     print('\n')
+
+
+                    elif short_code == 'fc':
+
+                            print("Search the credential")
+
+                            search_credentials = input()
+                            if check_existing_credentials(search_credentials):
+                                    search_credentials = find_credentials(search_credentials)
+                                    print(f"{search_credentials.username}")
+                                    print('-' * 20)
+
+                                    print(f"username.......{search_credentials}")
+                            else:
+                                    print("That credential does not exist")
+
+                    elif short_code == "ex":
+                            print("Bye .......")
+                            break
+                    else:
+                            print("I really didn't get that. Please use the short codes")
         
